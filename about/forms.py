14a1849404b22f19a5adebdb3a4b9bdeff9522cc
Mark Briscoe <mark@mcmw.co.uk>
2024-03-01 08:46:47 +0000

    first commit

---
+++ about/forms.py
@@ -0,0 +1,11 @@
+from .models import CollaborateRequest
+from django import forms
+
+
+class CollaborateForm(forms.ModelForm):
+    """
+    Form class for users to request a collaboration 
+    """
+    class Meta:
+        model = CollaborateRequest
+        fields = ('name', 'email', 'message')
---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault("DATABASE_URL", "postgres://xbjeiezz:MQCotYTHkANmhIZUvpKqt7zrZEnYDCfQ@trumpet.db.elephantsql.com/xbjeiezz",)
+os.environ.setdefault("SECRET_KEY", "pnkhadiofboi34905WJHOhaknlsrkWKGDS24OTHU0ETG08ONJ")
+os.environ.setdefault("CLOUDINARY_URL", "cloudinary://587534268699532:pMLNtYHhB1CH-kfOY054LErdscg@dqe5yeugm")
