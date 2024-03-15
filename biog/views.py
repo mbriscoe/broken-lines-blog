416bc9b4c398d5a46f0f5ff318cc797992751103
Mark Briscoe <mark@mcmw.co.uk>
2024-03-15 14:25:54 +0000

    update: Biog model  doc string

--- biog/views.py
+++ biog/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from .models import Biog
 
 def biog(request):
+    """
+    Renders the most recent list of biography entries
+
+    Displays instances of :model:'biog.Biog`.
+    **Context**
+    ``biogs``
+        The most recent instances of :model:`biog:Biog`.
     
+    **Template**
+    :template:`biog/biog_list.html`
+    """
     biogs = Biog.objects.all()
 
     return render(request, "biog/biog_list.html", {"biogs": biogs})
