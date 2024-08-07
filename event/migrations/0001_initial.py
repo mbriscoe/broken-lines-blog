81b58954b62b30e2c2ede360bf8679dd35addaa5
Mark Briscoe <mark.briscoe@codeinstitute.net>
2024-08-07 11:40:18 +0000

    reformat migration files to PEP8 standard

--- event/migrations/0001_initial.py
+++ event/migrations/0001_initial.py
@@ -14,7 +14,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Event',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(auto_created=True,
+                 primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200)),
                 ('description', models.TextField()),
                 ('date', models.DateField()),
